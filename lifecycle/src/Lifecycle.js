c26084018505371b906b411f410002b069aabc2a
Dil Sombayeva <dilyasom@gmail.com>
2019-09-05 06:00:46 -0700

    counter using CDU

--- lifecycle/src/Lifecycle.js
+++ lifecycle/src/Lifecycle.js
@@ -6,6 +6,8 @@ class Lifecycle extends Component {
   this.state = {
    str: 'In the constructor',
    counter: 0,
+   displayString: false,
+   increment: false,
   };
  }
 
@@ -14,10 +16,32 @@ class Lifecycle extends Component {
   console.log('in CDM');
   // setState(), forceupdate() triger Updating flow => render
   this.setState({ str: 'In the CDM' });
+  //   this.setState({ str: 'In the CDM', displayString: true });
  }
 
+ handleIncrement = () => {
+  this.setState({ displayString: true, increment: true });
+ };
+ handleDecrement = () => {
+  this.setState({ displayString: true, decrement: true });
+ };
  componentDidUpdate(prevProps, prevState) {
+  // setState() will lead to infinite loop (in Updating flow - setState() and forceUpdate() triger render)
   console.log('in CDU');
+  console.log('prevProps', prevProps);
+  console.log('prevState', prevState);
+  // we can use conditional statement to setState()
+  if (this.state.displayString) {
+   if (this.state.increment) {
+    this.setState({
+     counter: this.state.counter + 1,
+     displayString: false,
+     increment: false,
+    });
+   } else if (this.state.decrement) {
+    this.setState({ counter: this.state.counter - 1, decrement: false });
+   }
+  }
  }
 
  render() {
