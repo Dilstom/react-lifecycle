5d2ce63c0a34eb6e0e86005b1e8bda011a5f0e29
Dil Sombayeva <dilyasom@gmail.com>
2019-09-05 06:08:56 -0700

    toggle display

--- lifecycle/src/App.js
+++ lifecycle/src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Lifecycle from './Lifecycle';
 import './App.css';
 
-function App() {
- return (
-  <div className="App">
-   <h1>New Project</h1>
-   <Lifecycle />
-  </div>
- );
+class App extends Component {
+ constructor() {
+  super();
+  this.state = {
+   displayComponent: false,
+  };
+ }
+ handleDisplay = () => {
+  this.setState({ displayComponent: !this.state.displayComponent });
+ };
+
+ render() {
+  return (
+   <div className="App">
+    <h1>New Project</h1>
+    <button onClick={this.handleDisplay}>
+     {this.state.displayComponent ? 'Hide' : 'Display'} Component
+    </button>
+    {this.state.displayComponent ? <Lifecycle /> : null}
+   </div>
+  );
+ }
 }
 
 export default App;
